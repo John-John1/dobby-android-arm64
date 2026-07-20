170b6c2d5e08d60e1d5c7856ca6c11237dfeac12
yeshengmogu <3332720895@proton.me>
2026-07-20 23:26:18 +0800

    Fix linkage: remove extern C from stubs

--- source/TrampolineBridge/ClosureTrampolineBridge/arm64/closure_bridge_arm64.cc
+++ source/TrampolineBridge/ClosureTrampolineBridge/arm64/closure_bridge_arm64.cc
@@ -6,11 +6,11 @@
 
 // Stub trampoline code - just a RET instruction
 static uint8_t dummy_trampoline[] = { 0xc0, 0x03, 0x5f, 0xd6 };
-extern "C" uint8_t *closure_trampoline_asm = dummy_trampoline;
-extern "C" uint8_t *closure_trampoline_asm_end = dummy_trampoline + sizeof(dummy_trampoline);
+uint8_t *closure_trampoline_asm = dummy_trampoline;
+uint8_t *closure_trampoline_asm_end = dummy_trampoline + sizeof(dummy_trampoline);
 
 // Stub init function
-extern "C" void closure_bridge_init() {
+void closure_bridge_init() {
     // No-op for Android build without ASM
 }
 
