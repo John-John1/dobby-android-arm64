e887039a9f9e238d89aeb4e6c71451728c0aa8a2
yeshengmogu <3332720895@proton.me>
2026-07-23 18:43:05 +0800

    Fix Android ARM64 build: remove ASM, stub Cpu.h, fix ProcessRuntime

--- source/TrampolineBridge/ClosureTrampolineBridge/arm64/closure_bridge_arm64.cc
+++ source/TrampolineBridge/ClosureTrampolineBridge/arm64/closure_bridge_arm64.cc
@@ -1,17 +1,2 @@
-// Minimal stubs for Android ARM64 build
+// Stub - ASM removed, function defined in common_bridge_handler.h
 #include "../common_bridge_handler.h"
-#include <string.h>
-
-#if defined(__aarch64__)
-
-// Stub trampoline code - just a RET instruction
-static uint8_t dummy_trampoline[] = { 0xc0, 0x03, 0x5f, 0xd6 };
-uint8_t *closure_trampoline_asm = dummy_trampoline;
-uint8_t *closure_trampoline_asm_end = dummy_trampoline + sizeof(dummy_trampoline);
-
-// Stub init function
-void closure_bridge_init() {
-    // No-op for Android build without ASM
-}
-
-#endif
