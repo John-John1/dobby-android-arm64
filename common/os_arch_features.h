61420b2c97204ee56265bb36d391717109166010
yeshengmogu <3332720895@proton.me>
2026-07-23 19:16:51 +0800

    Fix os_arch_features.h: replace OSMemory with stub

--- common/os_arch_features.h
+++ common/os_arch_features.h
@@ -4,10 +4,6 @@
 #include <stddef.h>
 #include "pac_kit.h"
 
-// Include platform.h for OSMemory and MemoryPermission definitions
-// This avoids redefinition conflicts
-#include "PlatformUnifiedInterface/platform.h"
-
 #if defined(__arm64e__) && __has_feature(ptrauth_calls)
 #include <ptrauth.h>
 #endif
@@ -25,24 +21,19 @@ namespace apple {
 template <typename T> inline T arm64e_pac_strip(T &addr) {
   return pac_strip(addr);
 }
-
 template <typename T> inline T arm64e_pac_sign(T &addr) {
   return pac_sign(addr);
 }
-
 template <typename T> inline T arm64e_pac_strip_and_sign(T &addr) {
   return pac_strip_and_sign(addr);
 }
 } // namespace apple
 
 namespace android {
-// inline void make_memory_readable(void *address, size_t size) {
-#if defined(ANDROID)
-//  auto page = (void *)ALIGN_FLOOR(address, OSMemory::PageSize());
-//  if (!OSMemory::SetPermission(page, OSMemory::PageSize(), kReadExecute)) {
-//    return;
-  }
-#endif
+// Stub: make_memory_readable is not needed for Android build
+inline void make_memory_readable(void *address, size_t size) {
+  (void)address;
+  (void)size;
 }
 } // namespace android
 } // namespace features
